a3209d68f3e12803db0c5c8db42b10992f77bb7a
Igor Tsarkov <higorusama@gmail.com>
2019-01-17 21:37:47 +0300

    add queqe

--- builder.ps1
+++ builder.ps1
@@ -1,205 +1,192 @@
-# Microsoft AppCneter Full Access Authorize Token
-$api_token_FA = "457fb01333c32c88f493f5583641802470ae1105"
-# Microsoft AppCneter Read Only Token Authorize Token
-$api_token_RO = "5945e988813567c0ef6537caea8f5f327d95c10d"
-# Application name
-$app_name = "Damaten-CLub-App"
+# ver. 0.3
 
-# Check Folders
+# Microsoft AppCneter - Full Access Authorize Token
+[string] $apiTokenFA = "d2934eef6b48331fa66fc08b21e4911e7cc97f18"
+
+# Microsoft AppCneter - Read Only Authorize Token
+[string] $apiTokenRO = "5945e988813567c0ef6537caea8f5f327d95c10d"
+
+# Microsoft AppCneter - Application Name
+[string] $applicationName = "Damaten-CLub-App"
+
+[int] $sleepInSeconds = 10
+
+# File paths
+$logsPath = [IO.Path]::Combine($PSScriptRoot, 'logs')
+$buildsPath = [IO.Path]::Combine($PSScriptRoot, 'builds')
+
+$accessLogPath = [IO.Path]::Combine($PSScriptRoot, 'logs', 'access.log')
+$exceptionsLogPath = [IO.Path]::Combine($PSScriptRoot, 'logs', 'exceptions.log')
+$exceptionsTaskLogPath = [IO.Path]::Combine($PSScriptRoot, 'logs', 'exceptions_task.log')
+$taskDebugLogPath = [IO.Path]::Combine($PSScriptRoot, 'logs', 'taskDebug.log')
+
+# Report HEADER|FOOTER
+$reportHeader = "<style>
+table {border-collapse: collapse;}
+table, th, td {border: 1px solid #555;}
+td {width: 160px; padding: 2px;}
+</style>
+<h1>$($reportName)</h1>
+<table>
+    <tr>
+        <td><strong>Branch name</strong></td>
+        <td><strong>Build status</strong></td>
+        <td><strong>Duration</strong></td>
+        <td><strong>Link to build logs</strong></td>
+    </tr>`n"
+$reportFooter = "</table>"
+
+# Check (and Create) Folders
 function Create-Folders {
-$log_folder = Test-Path $PSScriptRoot\logs -PathType Container
-$builds_folder = Test-Path $PSScriptRoot\builds -PathType Container
+    [bool] $logFolder = Test-Path $logsPath -PathType Container
+    [bool] $buildsFolder = Test-Path $buildsPath -PathType Container
 
-    if ($log_folder -eq $FALSE) { mkdir $PSScriptRoot\logs }
-    if ($builds_folder -eq $FALSE) { mkdir $PSScriptRoot\builds }
+    if ($logFolder -eq $FALSE) { mkdir $logsPath ; "Folder LOGS created"}
+    if ($buildsFolder -eq $FALSE) { mkdir $buildsPath ; "Folder BUILDS created"}
 }
 
 # Connect to AppCenter API and return response
 function Get-API-Response {
-Param ([string]$api_url, [string]$method, [hashtable]$body)
+Param ([string]$APIUrl, [string]$method, [hashtable]$body)
 
-    # Api URL
-    $api_url = "https://api.appcenter.ms" + $api_url
+    # API url path
+    $APIUrl = "https://api.appcenter.ms" + $APIUrl
 
     # Headers
-    $headers = @{
+    [hashtable] $headers = @{
         "Accept" = "application/json"
-        "X-API-Token" = $api_token_FA
+        "X-API-Token" = $apiTokenFA
     }
 
-    # Init SSL connection
+    # Initiate SSL connection
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
     # If Authorized successfully, get/post response
     if(!$body) {
-        $response = Invoke-WebRequest -Uri $api_url -Method $method -Headers $headers
-    }
+        $response = Invoke-WebRequest -Uri $APIUrl -Method $method -Headers $headers }
     else {
         $headers.Add("Content-Type", "application/json")
-        $response = Invoke-WebRequest -Uri $api_url -Method $method -Headers $headers -Body ($body | ConvertTo-Json)
+        $response = Invoke-WebRequest -Uri $APIUrl -Method $method -Headers $headers -Body ($body | ConvertTo-Json)
     }
 
-    # $response
+    $dataTime = Get-Date -Format g
+
+    # If status 200, get JSON response
         if ($response.StatusCode -eq 200) {
             # Create JSON Content Section varitable
-            $content_json = $response.Content | ConvertFrom-Json
+            $contentJSON = $response.Content | ConvertFrom-Json
 
             # Write to access log
-            $data_time = Get-Date -Format g
-            $data_time + " | method: " + $method + " | url: " + $api_url | out-file $PSScriptRoot\logs\access.log -append }
+            $dataTime + " | method: " + $method + " | url: " + $APIUrl | out-file $accessLogPath -append }
         else {
-            "Error. Status Code: " + $response.StatusCode }
+            $erorrCode = "Error. Status Code: " + $response.StatusCode
+            $dataTime + " | " + $APIUrl + "Error " | out-file $accessLogPath -append }
 
-    return $content_json
+    return $contentJSON
 }
 
-# Create Report function
-function Create-Report {
-Param ([string]$app_name)
+# Builder function start task to build app in AppCenter and get status
+function Build-Task {
+Param ([string]$taskUserName, [string]$taskAppName, [string]$taskBranchName, [hashtable]$taskBuildBody, [string]$reportName)
+    Try {
+        $buildTaskResultJSON = Get-API-Response -APIUrl "/v0.1/apps/$($taskUserName)/$($taskAppName)/branches/$($taskBranchName)/builds" -method "Post" -body $taskBuildBody
 
-    # Get available branches
-    $raw_report_data = Get-API-Response -api_url "/v0.1/apps/$($user_json.name)/$($apps_json.name)/branches" -method "Get"
+        [string] $taskStatus = $buildTaskResultJSON.status
+        [string] $taskBuildId = $buildTaskResultJSON.id
 
-    # Init Date Format to REPORT file
-    $report_name = "Report-" + (Get-Date -UFormat %Y.%m.%d-%H.%M.%S)
+        do {
+            $res = Get-API-Response -APIUrl "/v0.1/apps/$($taskUserName)/$($taskAppName)/builds/$($taskBuildId)" -method "Get"
+            $taskStatus = $res.status
 
-    # REPORT table head
-    $table_head = "<style>
-    table {border-collapse: collapse;}
-    table, th, td {border: 1px solid #555;}
-    td {width: 160px; padding: 2px;}
-    </style>
-    <h1>$($report_name)</h1>
-    <table>
-        <tr>
-            <td><strong>Branch name</strong></td>
-            <td><strong>Build status</strong></td>
-            <td><strong>Duration</strong></td>
-            <td><strong>Link to build logs</strong></td>
-        </tr>`n" | out-file $PSScriptRoot\builds\$report_name.html -append
-
-    # Generate REPORT body
-    foreach($branch in $branches_json) {
-        # Build Duration
-        if ($branch.configured -ne $false) {
-            $duration = New-TimeSpan -Start $branch.lastBuild.startTime -End $branch.lastBuild.finishTime
-            $result = $branch.lastBuild.result
-            # Get link to logfile
-            $log_link = "<a href=`"https://appcenter.ms/download?url=/v0.1/apps/$($user_json.name)/$($app_name)/builds/$($branch.lastBuild.id)/downloads/logs`">Download log archive</a>"
-
+            Start-Sleep -Seconds $sleepInSeconds
         }
-        else {
-            $duration = "0"
-            $result = "not configured"
-            $log_link = "none"
+        until($taskStatus -eq "completed")
+        # Status Codes: [notStarted, inProgress, complited]
+
+
+        $duration = New-TimeSpan -Start $res.startTime -End $res.finishTime
+        $logLink = "https://appcenter.ms/download?url=/v0.1/apps/$($taskUserName)/$($taskAppName)/builds/$($res.id)/downloads/logs"
+
+        [hashtable] $buildResult = @{
+            "Branch" = $res.sourceBranch
+            "Result" = $res.result
+            "sourceVersion" = $res.sourceVersion
+            "Link" = $logLink
         }
 
-
-        # Table Row
-        $row = "<tr>
-            <td>$($branch.branch.name)</td>
-            <td>$($result)</td>
+        "<tr>
+            <td>$($res.sourceBranch)</td>
+            <td>$($res.result)</td>
             <td>$($duration)</td>
-            <td>$($log_link)</td>
-        </tr>`n"
-
-        # Write to file
-        $row | out-file $PSScriptRoot\builds\$report_name.html -append
-
+            <td><a href=`"$($logLink)`">Download log archive</a></td>
+         </tr>" | out-file $buildsPath\$reportName -append
+        $res| out-file $taskDebugLogPath -append
+        return $buildResult | ConvertTo-Json
     }
-
-    # REPORT table footer
-    $table_footer = "</table>" | out-file $PSScriptRoot\builds\$report_name.html -append
-
-    return "Report created"
+    Catch {
+    $exceptionMessage = $_.Exception.Message
+    $exceptionMessage | out-file $exceptionsTaskLogPath -append
+    }
 }
 
-# Test Async Task
-$async_task = {
-Param([string]$sourceVersion, [bool]$debug, [string]$user_name, [string]$app_name, [string]$branch_name)
-    # Body of build query
-    $build_body = @{
-        "sourceVersion" = $sourceVersion
-        "debug" = $debug
-    }
-    $build_branch = Get-API-Response -api_url "/v0.1/apps/$($user_name)/$($app_name)/branches/$($branch_name)/builds" -method "Post" -body $build_body
-    return $build_branch
-}
-
+# Start
 Try {
-
-# -----------------------
 # Create /logs and /builds folders
 Create-Folders
 
 # Get current user info
-$user_json = Get-API-Response -api_url "/v0.1/user" -method "Get"
-
-# Get available apps (I have now 1 app, not array >_> )
-$apps_json = Get-API-Response -api_url "/v0.1/apps" -method "Get"
+$userJSON = Get-API-Response -APIUrl "/v0.1/user" -method "Get"
 
 # Get available branches
-$branches_json = Get-API-Response -api_url "/v0.1/apps/$($user_json.name)/$($apps_json.name)/branches" -method "Get"
+$branchesJSON = Get-API-Response -APIUrl "/v0.1/apps/$($userJSON.name)/$($applicationName)/branches" -method "Get"
 
-# Generate hashtable: branch_name = (sourseVersion, 40-char string)
-[hashtable]$redy_to_build = @{}
-foreach($row in $branches_json) {
+# Set Report file name
+$reportName = "Report-" + (Get-Date -UFormat %Y.%m.%d-%H.%M.%S) + ".html"
+
+# Queue block
+#---------------------------------------------------------------
+$reportHeader | out-file $buildsPath\$reportName -append
+foreach($row in $branchesJSON) {
     if ($row.configured -ne $false) {
         # Get branch source version to build 
         $sourceVersion = $row.branch.commit.url.Substring($row.branch.commit.url.Length -40, 40)
 
-        # Add branch name and sourse version to Hashtable
-        $redy_to_build.Add($row.branch.name, $sourceVersion)
-
-
-         $build_body = @{
+        # Create Body for HTTP Request
+        $buildBody = @{
             "sourceVersion" = $sourceVersion
-            "debug" = $true
-         }
-         # notStarted, inProgress
-        $build_branch = Get-API-Response -api_url "/v0.1/apps/$($user_json.name)/$($apps_json.name)/branches/$($row.branch.name)/builds" -method "Post" -body $build_body
+            "debug" = $false }
+
+        # Run task to build App
+        $buildResult = Build-Task -taskUserName $userJSON.name -taskAppName $applicationName -taskBranchName $row.branch.name -taskBuildBody $buildBody -reportName $reportName
+        $buildResult
+
     }
 }
+$reportFooter | out-file $buildsPath\$reportName -append
+#---------------------------------------------------------------
 
-# DEBUG | log to file
-$redy_to_build | out-file $PSScriptRoot\logs\branches_avaible.txt -append
-
-# TEST | Async Job
-#Start-Job -ScriptBlock $async_task -ArgumentList "ac6c8e46065ecf6851436458ce621075b5fe8f0b", True, $user_json.name, $apps_json.name, "test_branch"
-
-Start-Sleep -Seconds 300
-Create-Report -app_name $apps_json.name
-
-"End Script"
-# -----------------------
-
+"Script complete, see report in BUILDS folder"
 }
 
 Catch {
 
-$data_time = Get-Date -Format g
-$exception_name = $_.Exception.GetType().Name
-$exception_message = $_.Exception.Message
+$dataTime = Get-Date -Format g
+$exceptionMessage = $_.Exception.Message
 
 # Create Error string
-$log_message = $data_time + " | " + $exception_message
+$logMessage = $dataTime + " | " + $exceptionMessage
 
 # Print exception
-$log_message
+$logMessage
 
 # Write to exceptions log
-$log_message | out-file $PSScriptRoot\logs\exceptions.log -append
+$logMessage | out-file $exceptionsLogPath -append
 
 }
 
 Finally {
-
-$response = $null
-$content_json = $null
-$branches_json = $null
-$branch = $null
-$file_dt = $null
-$redy_to_build = $null
-$build_body = $null
-
+$buildResult = $null
+$buildResult = $null
+$userJSON = $null
+$branchesJSON = $null
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,77 @@
-# DamatenClubAppBuilder
+# PowerShell Builder for Microsoft AppCenter
+Version: 0.3
+
+The script starts the application build process in the Microsoft Application Center.
+
+Script environment:  Windows 10 / PowerShell 5.1
+
+## Links
+[Microsoft AppCenter](https://appcenter.ms/)
+
+[Microsoft AppCenter API](https://openapi.appcenter.ms/#/)
+
+
+## Usage
+1. Enter the 40-character token. Token generated in AppCenter user settings. For example:
+```
+[string] $apiTokenFA = "d2934eef6b48331fa66fc08b21e4911e7cc97f18"
+```
+2. Enter the Application name.
+```
+[string] $applicationName = "Example-Application"
+```
+3. Enter the build cycle checking time in seconds.
+```
+[int] $sleepInSeconds = 10
+```
+4. Run script
+
+## Documentation
+
+### Before Try { } block:
+
+#### 1. Initialize variables
+
+[string] $apiTokenFA - Full Access Authorize Token
+
+[string] $apiTokenRO - Read Only Authorize Token
+
+[string] $applicationName - Microsoft AppCneter - Application Name
+
+[int] $sleepInSeconds - Build cycle checking time in seconds.
+
+Also set the pathes for logs and report templates.
+
+#### 2. Functions
+
+##### Create-Folders { }
+
+Check the availability of logs and reports directories . If absent - create it.
+
+##### Get-API-Response { }
+1. Set AppCenter API root url
+2. Set HTTP Headers varitable
+3. Initiate SSL connection (TLS 1.2)
+4. Choosing which method to use: GET of POST. If post - add "Content-Type"
+5. Write the Log
+6. Return JSON type Response
+
+##### Build-Task { }
+1. Use Try block
+2. Run the application build process by calling the function Get-API-Response
+3. Inside the loop, checking the build task has completed
+4. Write HTML report row in file
+5. Return result
+6. Catch exceptions if they occur
+
+### Inside Try { } блока
+1. Get the data of the user whose token was specified in [string] $apiTokenFA.
+2. Get a list of application repository branches specified in [string] $ applicationName
+3. Set Report file name
+4. In the foreach block we check that the branch is ready for building, get the sourceVersion from Url; form the body of the Post request, call the Build-Task
+
+### Inside Catch { } блока
+To catch exceptions and write to the log.
+
+### Inside Finally { } блока
+Reset some variables, to avoid errors when running the script in the same environment PowerShell
